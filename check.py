18dfe49865b53faea6455bd6faa9ffb957aa0b54
Sazib <mahbobhasan66@gmail.com>
2022-12-28 15:38:23 +0600

    Python added

---
+++ check.py
@@ -0,0 +1,12 @@
+# biriyaniprice=300
+# kabab=75
+# if biriyaniprice>300:
+#     print("Biriyani khabo")
+# elif kabab<90:
+#     print("Kabab khabo")
+# else:
+#     print("Butter Bon khabo cha diye")
+i=1
+while(i<3):
+    print(i,end=" ")
+    i+=1
